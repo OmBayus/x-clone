c295a631d8be97c91d039dc0cd8bb92a803cc8c0
ombayus <omerbayramcavus@gmail.com>
2023-11-01 02:27:15 +0300

    xclient and xserver init

---
+++ xserver/src/main/java/com/ombayus/xserver/XserverApplication.java
@@ -0,0 +1,13 @@
+package com.ombayus.xserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XserverApplication.class, args);
+    }
+
+}
