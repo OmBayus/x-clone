1a44699778415a500a160427701cdc0ab0baf150
Seda <sedanurgfb6671@hotmail.com>
2023-11-19 16:05:34 +0300

    secret

--- xserver/src/main/java/com/osntus/xserver/XserverApplication.java
+++ xserver/src/main/java/com/osntus/xserver/XserverApplication.java
@@ -1,14 +1,13 @@
 package com.osntus.xserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class XserverApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(XserverApplication.class, args);
-		System.out.println("seda");
 	}
 
 }
