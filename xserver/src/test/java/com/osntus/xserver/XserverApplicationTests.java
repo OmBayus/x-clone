b798464bcd2740487725a2359f95a71458ca5563
Seda <sedanurgfb6671@hotmail.com>
2023-11-19 15:37:25 +0300

    init server

---
+++ xserver/src/test/java/com/osntus/xserver/XserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osntus.xserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
