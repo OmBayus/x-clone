ab2b164227a936d2e366e6632784a62af60429d1
Seda <sedanurgfb6671@hotmail.com>
2023-12-12 22:41:14 +0300

    cleaned unused imports

--- xserver/src/main/java/com/osntus/xserver/XserverApplication.java
+++ xserver/src/main/java/com/osntus/xserver/XserverApplication.java
@@ -1,6 +1,5 @@
 package com.osntus.xserver;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- xserver/src/main/java/com/osntus/xserver/model/User.java
+++ xserver/src/main/java/com/osntus/xserver/model/User.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 
