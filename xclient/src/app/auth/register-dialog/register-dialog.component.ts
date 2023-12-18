db8eeed164a91d1cdc275ff5b861529b217e74ba
ombayus <omerbayramcavus@gmail.com>
2023-12-18 19:27:46 +0300

    feat: add all fields to register dialog

--- xclient/src/app/auth/register-dialog/register-dialog.component.ts
+++ xclient/src/app/auth/register-dialog/register-dialog.component.ts
@@ -9,11 +9,18 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class RegisterDialogComponent {
 
+  name: string = '';
   email: string = '';
+  username: string = '';
+  birthdate: Date | null = null;
   password: string = '';
 
   constructor(public dialogRef: MatDialogRef<RegisterDialogComponent>) { }
 
+  changeDate(event: any): void {
+    console.log(event.value);
+    this.birthdate = event.value;
+  }
 
   closeDialog(): void {
     this.dialogRef.close();
