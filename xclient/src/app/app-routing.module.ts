6daeae60ec4d5f5ae94cb20936eae9da9b5b3ae7
ombayus <omerbayramcavus@gmail.com>
2023-12-20 13:47:38 +0300

    feat: layouts module and sidebar component

--- xclient/src/app/app-routing.module.ts
+++ xclient/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: LandingComponent },
+    { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
