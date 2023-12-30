a06473d8a80b9cd3f193a992667bce9fd088e2f9
ombayus <omerbayramcavus@gmail.com>
2023-12-30 13:36:04 +0300

    feat: sidebar navigation

--- xclient/src/app/layouts/sidebar/sidebar.component.ts
+++ xclient/src/app/layouts/sidebar/sidebar.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-
+  navigationList = [
+    {
+      name: 'Home',
+      route: '/home',
+      icon: 'home'
+    },
+    {
+      name: 'Explore',
+      route: '/explore',
+      icon: 'explore'
+    },
+    {
+      name: "Notifications",
+      route: '/notifications',
+      icon: 'notifications'
+    },
+    {
+      name: 'Messages',
+      route: '/messages',
+      icon: 'message'
+    },
+    {
+      name: 'Profile',
+      route: '/profile',
+      icon: 'person'
+    },
+    {
+      name: 'More',
+      route: '/more',
+      icon: 'more_horiz'
+    }
+  ]
 }
